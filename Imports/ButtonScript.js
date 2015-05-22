134981701afe64c98a84829da84d2e36fa58fb84
mooimsarah <spentek7@students.d125.org>
2015-05-22 09:49:04 -0500

    Menu

---
+++ Imports/ButtonScript.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+var anim : Animation;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function LoadLevel(){
+	Debug.Log("Load Tha Level");
+}
+
+function ClickStart(){
+	animation.Play();
+	StartCoroutine("WaitBeforeLoad");
+}
+
+function WaitBeforeLoad(){
+	yield WaitForSeconds(1);
+	LoadLevel();
+}
+
+function ClickInstructions(){
+	animation.Play();
+	anim.Play("Move in instruct");
+}
+
+function Back(){
+	anim.Play("Move out instruct");
+	animation.Play("Move in");
+}
+
+function ClickedCredits(){
+	Debug.Log("Load Credits");
+}
